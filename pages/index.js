045ebe937875fc7f63e6c19b95768bb90b52426b
yhotta <hotta-nagoya-6163@au.com>
2022-05-08 11:56:43 +0000

    自己紹介文を記入

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,11 @@ const Home = ({ allPostsData }) => {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>自己紹介文</p>
+        <p>
+          はじめまして。<br />
+          エンジニア就活をしている過程でNext.jsのチュートリアルを行いました。<br />
+          まだまだReactやNext.jsについてわからない部分が多いので、今後も勉強頑張ります。
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
